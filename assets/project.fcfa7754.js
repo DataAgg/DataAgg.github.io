b9c153f03eee46fae4024a7bbe004a3d134a6a9b
watano <watano@qq.com>
2022-12-06 20:12:16 +0800

    fix bugs

---
+++ assets/project.fcfa7754.js
@@ -0,0 +1,1 @@
+import{_ as t,r as n,o,c,d as a}from"./index.68ff1ca1.js";const s={};function l(e,u){const r=n("router-view");return o(),c("main",null,[(o(),a(r,{key:e.$route.fullPath,route:e.$route},null,8,["route"]))])}const f=t(s,[["render",l]]);export{f as default};
